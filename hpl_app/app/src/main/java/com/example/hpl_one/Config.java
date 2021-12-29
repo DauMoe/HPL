7703aabdca450952a9e9aaf2fa630ea6801605ad
DauMoe <lehuuyhoang1111999@gmail.com>
2021-12-29 16:30:53 +0700

    add cmt

--- hpl_app/app/src/main/java/com/example/hpl_one/Config.java
+++ hpl_app/app/src/main/java/com/example/hpl_one/Config.java
@@ -1,6 +1,8 @@
 package com.example.hpl_one;
 
 public class Config {
+    //Read Multipart: https://stackoverflow.com/questions/54133578/how-to-parse-response-from-multipart-form-data-request-in-retrofit2-android/54136872
+
     //Login pref
     public static final String LOGIN_STATE  = "LOGIN_STATE";
     public static final String SSID         = "sessionID";
